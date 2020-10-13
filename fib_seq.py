b5322fe7deca60d3082b068a1f13923f0bef960c
jonathanFielder <50757815+jonathanFielder@users.noreply.github.com>
2020-10-13 15:16:17 -0400

    Add files via upload

---
+++ fib_seq.py
@@ -0,0 +1,25 @@
+#an attempt at a pure functional Fib sequence that returns a list and no side effects
+def fib_to(x):
+    results = []
+    i = 0
+    a = 0
+    b = 0
+    while i < x:
+        #below is to test a and b
+        #print('a' + str(a))
+        #print('b' + str(b))
+        n = (a) + (b)
+        if a < 1:
+            a += 1
+        elif i % 2 != 0:
+            a = n
+        else:
+            b = n
+        i += 1
+        results.append(n)
+    return results
+
+while True:
+    print(fib_to(int(input('Enter number to generate Fib: '))))
+    
+    
